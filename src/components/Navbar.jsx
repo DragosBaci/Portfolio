bee243f91d0b0ef51f8e4780ffaf39fe0b78d571
DragosBaci <dragos6171@gmail.com>
2023-03-13 20:47:39 +0200

    final fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = ({ nav, handleClick }) => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300 "
-              href="https://github.com/DragosBaci/Portfolio/raw/main/public/resume/CV.pdf"
+              href="https://github.com/DragosBaci/"
             >
               Github <FaGithub size={30} />
             </a>
@@ -112,7 +112,7 @@ const Navbar = ({ nav, handleClick }) => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300 "
-              href="/resume/CV.pdf"
+              href="https://github.com/DragosBaci/Portfolio/raw/main/public/resume/CV.pdf"
               download
             >
               Resume <BsFillPersonLinesFill size={30} />
